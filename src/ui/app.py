fba19c45d570d4c7955133460e55e0dd13b88f52
Gopo23 <gourav.kumar.lal23@gmail.com>
2026-08-08 18:10:34 +0530

    Restore try-except block for pysqlite3 to fix local ModuleNotFoundError

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,6 +1,10 @@
 import sys
-__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 
 from pathlib import Path
 import streamlit as st
